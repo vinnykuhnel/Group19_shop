6b00582e0bc523687868ff0a1165d3858d0d297b
OllieB <55852081+OTB8@users.noreply.github.com>
2021-12-01 14:09:51 -0600

    Add files via upload
    Changed menu display, added print statements as well as fixed spacing throughout the code.   Verified each command is still working properly.

--- shop.py
+++ shop.py
@@ -7,30 +7,33 @@ from data import InitializeDB
 
 connection = InitializeDB()
 
+
 @dataclass
 class cartclass:
     def __init__(self, pk, userID, ordered, total):
-        #primary key of cart id in db
+        # primary key of cart id in db
         cartclass.pk = pk
         cartclass.userID = userID
-        #1 or 0 to represent ordered status
+        # 1 or 0 to represent ordered status
         cartclass.ordered = ordered
         cartclass.total = total
 
+
 @dataclass
-#represents an entry in the cart item database table
+# represents an entry in the cart item database table
 class cartitemclass:
     def __init__(self, pk, cartID, movieID, title, quantity):
-        #primary key of DB entry
+        # primary key of DB entry
         self.pk = pk
-        #cart the entry corresponds to
+        # cart the entry corresponds to
         self.cartID = cartID
-        #movie referenced by cart entry
+        # movie referenced by cart entry
         self.movieID = movieID
-        #title of movie
+        # title of movie
         self.title = title
-        #quantity of movies
+        # quantity of movies
         self.quantity = quantity
+
 
 @dataclass
 class accountclass:
@@ -44,6 +47,7 @@ class accountclass:
         accountclass.shippingAddr = shippingAddr
         accountclass.billingAddr = billingAddr
 
+
 @dataclass
 class movieclass:
     def __init__(self, pk, serial, title, price, rating, genre, quantity):
@@ -55,31 +59,30 @@ class movieclass:
         self.genre = genre
         self.quantity = quantity
 
+
 class inventory:
-    def __init__(self):        
+    def __init__(self):
         self.inventory: movieclass = []
-        
+
     def UpdateInventoryList(self):
         self.inventory.clear()
-        #SQL Query to get all avalable movies
+        # SQL Query to get all avalable movies
         result = connection.execute("SELECT rowid, * FROM Movie").fetchall()
         for item in result:
             movie = movieclass(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
-            self.inventory.append(movie)        
+            self.inventory.append(movie)
 
     def viewInventory(self):
-                
+
         for item in self.inventory:
-            print("\n")
-            print ("Serial: " + item.serial)
+            print("Serial: " + item.serial)
             print("Title: " + item.title)
             print("Price: " + str(item.price))
             print("Rating: " + item.rating)
             print("Genre: " + item.genre)
-            print("Quantity: " + str(item.quantity))
-            print("\n")
+            print("Quantity: " + str(item.quantity),"\n")
 
-    def updateInventory(self, pk, quantity, remove = False):
+    def updateInventory(self, pk, quantity, remove=False):
         movie = None
         for item in self.inventory:
             if item.pk == pk:
@@ -94,10 +97,10 @@ class inventory:
             connection.commit()
             return (movie.price * quantity)
         else:
-            #May not be needed, discuss with group
+            # May not be needed, discuss with group
             # SQL statement to increase item's quantity by quantity parameter
             return 0
-    
+
     def checkInventory(self, movieName, quantity):
         movieCheck = None
         for item in self.inventory:
@@ -108,67 +111,64 @@ class inventory:
                 return movieCheck.pk
         return 0
 
-
-
 class cart:
     def __init__(self):
         self.entries: cartitemclass = []
         self.cart: cartclass = None
-    
+
     def assignCart(self, userID):
-        #check for a current non-ordered cart corresponding to user
+        # check for a current non-ordered cart corresponding to user
         tuple = (userID, 0)
         queryStr = '''SELECT rowid, * FROM Cart WHERE userID=? AND ordered=?'''
         result = connection.execute(queryStr, tuple).fetchone()
-        if result:                        
+        if result:
             self.cart = cartclass(result[0], result[1], result[2], result[3])
-            
+
         else:
-            
+
             self.newCart(userID)
         self.getItems()
 
-    
     def newCart(self, userID):
-        #create a new cart for certain user
+        # create a new cart for certain user
         tuple = (userID, 0, 0)
         queryStr = '''INSERT INTO Cart VALUES (?, ?, ?)'''
         connection.execute(queryStr, tuple)
         connection.commit()
         result = connection.execute("SELECT last_insert_rowid();").fetchone()
-        
+
         self.cart = cartclass(result[0], userID, 0, 0)
-    
+
     def getItems(self):
         self.entries: cartitemclass = []
-        #Add all entries in Cart Items table to list
+        # Add all entries in Cart Items table to list
         tuple = (self.cart.pk,)
         queryStr = '''SELECT rowid, * FROM CartItem WHERE cartID=?'''
         result = connection.execute(queryStr, tuple).fetchall()
         for item in result:
-            self.entries.append(cartitemclass(item[0], item[1], item[2], item[3], item[4]))        
-    
-    def viewCart(self):
-        #display items in current cart
-        print("Items in your Cart: ")
-        for i in self.entries:
-            print ("Entry Num: " + str(i.pk) + " Title: " + i.title + " Quantity: " + str(i.quantity))
+            self.entries.append(cartitemclass(item[0], item[1], item[2], item[3], item[4]))
 
-    def addToCart(self, movieID, movieTitle, quantity):             
-        
+    def viewCart(self):
+        # display items in current cart
+        print("\nItems in your Cart: ")
+        for i in self.entries:
+            print("Entry Num: " + str(i.pk) + " Title: " + i.title + " Quantity: " + str(i.quantity))
+        print("\n")
+    def addToCart(self, movieID, movieTitle, quantity):
+
         if movieID > 0:
-            #SQL statement to add a entry in cart item table
+            # SQL statement to add a entry in cart item table
             tuple = (self.cart.pk, movieID, movieTitle, quantity)
             queryStr = '''INSERT INTO CartItem VALUES (?, ?, ?, ?)'''
             connection.execute(queryStr, tuple)
             connection.commit()
             return True
-            
+
         else:
             return False
-        
 
     def removeFromCart(self, itemID):
+
         connection.execute("DELETE FROM CartItem WHERE rowid=?", (itemID,))
         connection.commit()
 
@@ -176,15 +176,14 @@ class cart:
         connection.execute('''UPDATE Cart SET ordered = 1, total = ? WHERE rowid=?''', (total, self.cart.pk))
         connection.commit()
         self.entries.clear()
-        
 
 
 class account():
     def __init__(self):
         self.account: accountclass = None
-        
+
     def createAccount(self, fName, lName, username, password, creditCard, shippingAddr, billingAddr):
-        #insert new account in db
+        # insert new account in db
         tuple = (fName, lName, username, password, creditCard, shippingAddr, billingAddr)
         queryStr = '''INSERT INTO Account VALUES (?, ?, ?, ?, ?, ?, ?)'''
         try:
@@ -192,11 +191,10 @@ class account():
             connection.commit()
         except:
             pass
-    
+
     def deleteAccount(self):
         connection.execute("DELETE FROM Account WHERE rowid=?", (self.account.id,))
         connection.commit()
-        
 
     def editShippingInfo(self):
         newAddr = input(str("Enter your new Shipping Address: "))
@@ -223,26 +221,24 @@ class account():
         connection.commit()
         self.account.password = newPassword
 
-
     def authenticate(usernameEntered, enteredPassword):
-        #query for usernames record
+        # query for usernames record
         queryStr = '''SELECT rowid, * FROM Account WHERE username=?'''
         result = connection.execute(queryStr, (usernameEntered,)).fetchone()
-        
-        if result:    
+
+        if result:
             if result[4] == enteredPassword:
-                account = accountclass(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7])
+                account = accountclass(result[0], result[1], result[2], result[3], result[4], result[5], result[6],
+                                       result[7])
                 return account
             else:
-                return None            
-                
-        
+                return None
 
 
 class orderHistory:
     def addToOrderHistory():
         # SQL statement to make a new cart
-        # SQL statement to update current cart to new cart 
+        # SQL statement to update current cart to new cart
         return 0
 
     def displayOrderHistory(self, userID):
@@ -254,85 +250,82 @@ class orderHistory:
                 print("Order #: " + str(i[0]) + " Title: " + i[2] + "  Quantity: " + str(i[3]))
             if cart:
                 print("TOTAL: " + str(item[1]))
-        
-
-
-
-
-
 
 
 def parser(string, movies: inventory, orders: orderHistory, userCart: cart, user: account, session: bool):
-        #Initialize or update the cart/inventory user will be working with
-        userCart.assignCart(user.account.id)
-        movies.UpdateInventoryList()
-        
+    # Initialize or update the cart/inventory user will be working with
+    userCart.assignCart(user.account.id)
+    movies.UpdateInventoryList()
 
-        #match typed command to action
-        if string == "view items":
-            movies.viewInventory()
-        elif string == "view cart":
-            userCart.viewCart()
-        elif string == "add cart item":
-            
-            movieName = input(str("Enter the name of the movie: "))
-            quantity = int(input("Enter the number of copies: "))
-            
-            if userCart.addToCart(movies.checkInventory(movieName, quantity), movieName, quantity):
-                print("Item was Successfilly added to cart!")
-            else:
-                print("Enter a valid title and quantity!")
-                
-        elif string == "remove from cart":
-            cartID = input(str("Enter cart Num you would like to remove: "))
-            userCart.removeFromCart(cartID)
-        elif string ==  "checkout":
-            validCart = True
-            total = 0
-            for item in userCart.entries:
-                if movies.checkInventory(item.title, item.quantity) > 0:
-                    continue
-                else:
-                    validCart = False
-            if validCart:
-                for item in userCart.entries:
-                    #Update number in inventory
-                    total += movies.updateInventory(item.movieID, item.quantity, True)
-                #change cart entry to ordered
-                print(total)
-                userCart.checkout(total)
-        elif string ==  "add order to history":
-            orders.addOrderHistory()
-        elif string == "view order history":
-            orders.displayOrderHistory(user.account.id)
-        elif string == "edit account":
-            user.editPassword()
-            user.editPaymentInfo()
-            user.editShippingInfo()
-        elif string ==  "delete account":
-            user.deleteAccount()
-            session = False
-        elif string ==  "logout":
-            session = False
+    # match typed command to action
+    if string == "View items":
+        movies.viewInventory()
+    elif string == "View cart":
+        userCart.viewCart()
+    elif string == "Add cart item":
+
+        movieName = input(str("Enter the title of the movie that you would like to add: "))
+        quantity = int(input("Enter the number of copies of {} that you would like to purchase:  ".format(movieName)))
+
+        if userCart.addToCart(movies.checkInventory(movieName, quantity), movieName, quantity):
+            print("\nThe movie title '{}' was successfully added to cart with a quantity of {}!\n".format(movieName,quantity))
         else:
-            print("Error: Command invalid")
-        return movies, orders, userCart, user, session
-    
-def main():   
-    
+            print("\nError! You did not enter either a valid title or quantity. Please try again.\n")
+
+    elif string == "Remove from cart":
+        for i in self.entries:
+            print("Entry Num: " + str(i.pk) + " Title: " + i.title + " Quantity: " + str(i.quantity))
+        print("\n")
+        cartID = input(str("\nEnter cart Num you would like to remove: "))
+        userCart.removeFromCart(cartID)
+    elif string == "Checkout":
+        validCart = True
+        total = 0
+        for item in userCart.entries:
+            if movies.checkInventory(item.title, item.quantity) > 0:
+                continue
+            else:
+                validCart = False
+        if validCart:
+            for item in userCart.entries:
+                # Update number in inventory
+                total += movies.updateInventory(item.movieID, item.quantity, True)
+            # change cart entry to ordered
+            print("Checkout successful. The total of all movies in the cart was ", total, ".\n")
+            userCart.checkout(total)
+    elif string == "Add order to history":
+        orders.addOrderHistory()
+    elif string == "View order history":
+        orders.displayOrderHistory(user.account.id)
+        print("\n")
+    elif string == "Edit account":
+        user.editPassword()
+        user.editPaymentInfo()
+        user.editShippingInfo()
+        print("\n")
+    elif string == "Delete account":
+        user.deleteAccount()
+        session = False
+    elif string == "Logout":
+        session = False
+    else:
+        print("Error! You enter an invail command. Please try again.\n")
+    return movies, orders, userCart, user, session
+
+
+def main():
     while 1:
         movies = inventory()
         orders = orderHistory()
         userCart = cart()
         user: account = None
 
-
-        print("Welcome")
+        print("Welcome to The Movie Shop!")
         while 1:
-            loginChoice = input(str("login or create account? "))
+            loginChoice = input(str("Would you like to login or create an account? "))
             if loginChoice == 'login':
-                username = input(str("Username: "))            
-                password = input(str("Password: "))
+                username = input(str("Please enter your username: \n"))
+                password = input(str("Please enter your password: \n"))
 
                 acc: accountclass = account.authenticate(username, password)
                 user = account()
@@ -340,31 +333,29 @@ def main():
                 if user.account:
                     break
                 else:
-                    print("Error: Username and password is incorrect")
+                    print("Error! The username and password combination does not exist. \nPlease try again or create an account. \n")
                     continue
-            elif loginChoice == 'create account':
-                fName = input(str("Enter your first name: "))
-                lName = input(str("Enter your last name "))
-                username = input(str("Enter New Username: "))
-                password = input(str("Enter Password: "))
-                creditCard = input(str("Enter card number: "))
-                shippingAddr = input(str("Enter your Shipping Address: "))
-                billingAddr = input(str("Enter your Billing Addrress: "))
+            elif loginChoice == 'create an account':
+                fName = input(str("Please enter your first name: "))
+                lName = input(str("Please enter your last name: "))
+                username = input(str("Please enter a new username: "))
+                password = input(str("Please enter a password: "))
+                creditCard = input(str("Please enter your credit/debit card number: "))
+                shippingAddr = input(str("Please enter your Shipping Address: "))
+                billingAddr = input(str("Please enter your Billing Addrress: "))
                 user = account()
                 user.createAccount(fName, lName, username, password, creditCard, shippingAddr, billingAddr)
                 continue
             else:
                 continue
-        session = True        
-        print("Authentication successful: ")
+        session = True
+        print("\nAuthentication and login were successful. \n")
         while session:
-            print("Options => view items, view cart, add cart item, remove from cart, checkout, view order history, edit account, delete account, logout")
-            command = input(str("Enter one of the commands above: "))
+            print(
+                "MENU OPTIONS => \n View items \n View cart \n Add cart item \n Remove from cart \n Checkout \n View order history \n Edit account \n Delete account \n Logout \n")
+            command = input(str("Please enter a command from the above list to  perform: "))
             movies, orders, userCart, user, session = parser(command, movies, orders, userCart, user, session)
 
 
-if __name__=="__main__":
-    
-    
+if __name__ == "__main__":
     main()
-
